ebc0db6d522a9b39641332e0688e4ca3cfc920b6
Brandon Emerson <Brandon.Emerson77@gmail.com>
2017-07-12 22:35:29 -0400

    update: header in progress

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+var $ = require('jquery');
+
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    console.log(winTop);
+    if (winTop >= 30) {
+      $("body").addClass("sticky-header");
+    } else {
+      $("body").removeClass("sticky-header");
+    }
+  })
+})
