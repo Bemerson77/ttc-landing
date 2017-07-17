1b47a78d58876fe9ce2f525bffa2aea8cbb1afdd
Brandon Emerson <Brandon.Emerson77@gmail.com>
2017-07-16 21:50:09 -0400

    update

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,17 +3,17 @@ var $ = require('jquery');
 var headHeight = $('.header-outer').height();
 var topDecimal = (0.18 / 100) * headHeight;
 var topVal = (topDecimal * 100).toFixed();
-$('.header-banner-inner').css('top', topVal + "px");
+$('.header-banner-inner').css('top', topVal + 'px');
 
-$( window ).resize(function() {
-  var headHeight = $('.header-outer').height();
-  var topDecimal = (0.18 / 100) * headHeight;
-  var widthDecimal = (0.81 / 100) * headHeight;
-  var topVal = (topDecimal * 100).toFixed();
-  var widthVal = (widthDecimal * 100).toFixed();
-  console.log(widthVal);
-  // console.log(topVal);
-  $('.header-banner-inner').css('top', topVal + "px");
+$(window).resize(function() {
+    var headHeight = $('.header-outer').height();
+    var topDecimal = (0.18 / 100) * headHeight;
+    var widthDecimal = (0.81 / 100) * headHeight;
+    var topVal = (topDecimal * 100).toFixed();
+    var widthVal = (widthDecimal * 100).toFixed();
+    console.log(widthVal);
+    // console.log(topVal);
+    $('.header-banner-inner').css('top', topVal + 'px');
 });
 
 
@@ -28,10 +28,10 @@ $(function() {
         // console.log(headerHeight);
         // console.log(winTop);
 
-        if(winTop >= visionTop - 100) {
-          $('body').addClass('sticky-header');
+        if (winTop >= visionTop - 100) {
+            $('body').addClass('sticky-header');
         } else {
-          $('body').removeClass('sticky-header');
+            $('body').removeClass('sticky-header');
         }
 
     });
