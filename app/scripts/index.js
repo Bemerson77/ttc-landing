34b2c9c0b13fae8708f2e46feb67a50f8047aec5
Brandon Emerson <Brandon.Emerson77@gmail.com>
2017-07-23 22:10:06 -0400

    working on footer: added new white pngs for footer

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,18 +1,18 @@
 var $ = require('jquery');
 
-var headHeight = $('.header-outer').height();
-var topDecimal = (0.18 / 100) * headHeight;
-var topVal = (topDecimal * 100).toFixed();
-$('.header-banner-inner').css('top', topVal + 'px');
-
-$(window).resize(function() {
-    var headHeight = $('.header-outer').height();
-    var topDecimal = (0.18 / 100) * headHeight;
-    var widthDecimal = (0.81 / 100) * headHeight;
-    var topVal = (topDecimal * 100).toFixed();
-    var widthVal = (widthDecimal * 100).toFixed();
-    $('.header-banner-inner').css('top', topVal + 'px');
-});
+// var headHeight = $('.header-outer').height();
+// var topDecimal = (0.18 / 100) * headHeight;
+// var topVal = (topDecimal * 100).toFixed();
+// $('.header-banner-inner').css('top', topVal + 'px');
+//
+// $(window).resize(function() {
+//     var headHeight = $('.header-outer').height();
+//     var topDecimal = (0.18 / 100) * headHeight;
+//     var widthDecimal = (0.81 / 100) * headHeight;
+//     var topVal = (topDecimal * 100).toFixed();
+//     var widthVal = (widthDecimal * 100).toFixed();
+//     $('.header-banner-inner').css('top', topVal + 'px');
+// });
 
 
 $(function() {
@@ -29,4 +29,20 @@ $(function() {
         }
 
     });
+
+    var body = $('body');
+    var backgrounds = [
+      'url(http://static.jsbin.com/images/jsbin_static.png)',
+      'url(http://static.jsbin.com/images/popout.png)'];
+    var current = 0;
+
+    function nextBackground() {
+        body.css(
+            'background',
+        backgrounds[current = ++current % backgrounds.length]);
+
+        setTimeout(nextBackground, 5000);
+    }
+    setTimeout(nextBackground, 5000);
+    body.css('background', backgrounds[0]);
 });
--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -2,19 +2,19 @@
 "use strict";
 var $ = require('jquery');
 
-var headHeight = $('.header-outer').height();
-var topDecimal = (0.18 / 100) * headHeight;
-var topVal = (topDecimal * 100).toFixed();
-$('.header-banner-inner').css('top', topVal + 'px');
-
-$(window).resize(function() {
-    var headHeight = $('.header-outer').height();
-    var topDecimal = (0.18 / 100) * headHeight;
-    var widthDecimal = (0.81 / 100) * headHeight;
-    var topVal = (topDecimal * 100).toFixed();
-    var widthVal = (widthDecimal * 100).toFixed();
-    $('.header-banner-inner').css('top', topVal + 'px');
-});
+// var headHeight = $('.header-outer').height();
+// var topDecimal = (0.18 / 100) * headHeight;
+// var topVal = (topDecimal * 100).toFixed();
+// $('.header-banner-inner').css('top', topVal + 'px');
+//
+// $(window).resize(function() {
+//     var headHeight = $('.header-outer').height();
+//     var topDecimal = (0.18 / 100) * headHeight;
+//     var widthDecimal = (0.81 / 100) * headHeight;
+//     var topVal = (topDecimal * 100).toFixed();
+//     var widthVal = (widthDecimal * 100).toFixed();
+//     $('.header-banner-inner').css('top', topVal + 'px');
+// });
 
 
 $(function() {
@@ -31,6 +31,22 @@ $(function() {
         }
 
     });
+
+    var body = $('body');
+    var backgrounds = [
+      'url(http://static.jsbin.com/images/jsbin_static.png)',
+      'url(http://static.jsbin.com/images/popout.png)'];
+    var current = 0;
+
+    function nextBackground() {
+        body.css(
+            'background',
+        backgrounds[current = ++current % backgrounds.length]);
+
+        setTimeout(nextBackground, 5000);
+    }
+    setTimeout(nextBackground, 5000);
+    body.css('background', backgrounds[0]);
 });
 
 },{"jquery":2}],2:[function(require,module,exports){
